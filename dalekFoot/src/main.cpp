c3bda9f09b49751217aa56372cec7dd2813e32f0
HE Qichen <heqichen@gmail.com>
2021-02-28 19:16:52 +0000

    ready to read radar information

---
+++ dalekFoot/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "sensor/ars4b.h"
+
+
+
+int main(void) {
+	cout << "Hello World!" << endl;
+
+	Radar *ars4b = new Ars4b;
+
+	return 0;
+}
---
+++ dalekFoot/src/sensor/ars4b.h
@@ -0,0 +1,14 @@
+#ifndef __ARS4B_H__
+#define __ARS4B_H__
+
+class Radar {
+	
+};
+
+class Ars4b: public Radar {
+
+};
+
+
+#endif
+
