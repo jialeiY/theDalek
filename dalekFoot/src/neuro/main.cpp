253d8c156e3fc4da5f78b8fb03e545aba3cb3dd1
HE Qichen <heqichen@gmail.com>
2021-05-14 01:43:23 +0800

    ready to read command

--- dalekFoot/src/neuro/main.cpp
+++ dalekFoot/src/neuro/main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
 #include "framework/thread_hub.h"
 #include "framework/timer_thread.h"
 #include "framework/watchdog_thread.h"
 #include "framework/io_thread.h"
+#include <numeric>
 
 using namespace std;
+
+
+vector<string> splitString(const string &str) {
+	vector<string> ret;
+	size_t found = string::npos;
+	size_t last = 0;
+	while ((found = str.find(' ', last)) != std::string::npos) {
+    ret.push_back(move(str.substr(last, (found - last))));
+		last = found + 1;
+	}
+	ret.push_back(str.substr(last, str.size() - last));
+	return ret;
+}
 
 int main() {
 	ThreadHub th;
@@ -22,8 +37,18 @@ int main() {
 	iot.start();
 	tt.start();
 
-	while (true)
-		;
+	string cmd;
+
+	while (true) {
+		getline(cin, cmd);
+		vector<string> cmdList = splitString(cmd);
+		string out = accumulate(next(begin(cmdList)), end(cmdList), cmdList.front(), [](string acc, const string &item) {
+			return acc + " - " + item;
+		});
+		cout << out << endl;		
+
+	}
+		
 
 	// char buffer[256];
 
