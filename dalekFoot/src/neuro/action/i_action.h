a1b26b277d03df552d642f7af68b130031a6c37d
HE Qichen <heqichen@gmail.com>
2021-05-24 22:43:31 +0800

    add action

---
+++ dalekFoot/src/neuro/action/i_action.h
@@ -0,0 +1,19 @@
+#ifndef __ACTION_I_ACTION_H__
+#define __ACTION_I_ACTION_H__
+
+
+namespace action {
+
+
+
+class IAction {
+	public:
+		IAction() {};
+		virtual ~IAction() {};
+};
+
+
+}
+
+
+#endif
--- dalekFoot/src/neuro/framework/entity_agency.h
+++ dalekFoot/src/neuro/framework/entity_agency.h
@@ -2,6 +2,7 @@
 #define __FRAMEWORK_ENTITY_AGENCY_H__
 
 #include "hardware/i_hardware.h"
+#include "action/i_action.h"
 #include <string>
 #include <unordered_map>
 
@@ -10,9 +11,11 @@ public:
 	EntityAgency();
 	virtual ~EntityAgency();
 	inline void registerHardware(const std::string &name, hardware::IHardware *hardware) { mHardwareMap[name] = hardware; }
+	inline void registerAction(const std::string &name, action::IAction *action) {mActionMap[name] = action;}
 	hardware::IHardware *getHardware(const std::string &name) const;
 private:
 	std::unordered_map<std::string, hardware::IHardware *> mHardwareMap;
+	std::unordered_map<std::string, action::IAction *> mActionMap;
 };
 
 #endif
