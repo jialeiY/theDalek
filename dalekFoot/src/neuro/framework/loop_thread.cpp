a8992f4fcc20418b60d347cc458f091054658ba2
HE Qichen <heqichen@gmail.com>
2021-05-26 18:02:14 +0800

    WIP: modify loop thread

--- dalekFoot/src/neuro/framework/loop_thread.cpp
+++ dalekFoot/src/neuro/framework/loop_thread.cpp
@@ -14,9 +14,12 @@ void LoopThread::onNotify(EventType msgType, volatile void *data) {
 
 }
 
-
+/// @brief This is the main loop controller to contorl the time interval of 
+/// sensring and control calculation. 
+/// work() function intented to return fast to not block the IO. and not block
+/// being notified.
 void LoopThread::work() {
-
+	
 }
 
 
---
+++ dalekFoot/src/neuro/parameter.h
@@ -0,0 +1,8 @@
+#ifndef __PARAMETER_H__
+#define __PARAMETER_H__
+
+#include <chrono>
+
+
+
+#endif
