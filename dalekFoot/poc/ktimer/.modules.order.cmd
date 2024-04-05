d0f51618e728e23b537cfc2e588aa4b3aae032ee
HE Qichen <heqichen@gmail.com>
2024-04-05 11:32:18 +0000

    add timer in kernel test code

---
+++ dalekFoot/poc/ktimer/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/heqichen/workspace/theDalek/dalekFoot/poc/ktimer/modules.order := {   echo /home/heqichen/workspace/theDalek/dalekFoot/poc/ktimer/ktimer.ko; :; } > /home/heqichen/workspace/theDalek/dalekFoot/poc/ktimer/modules.order
