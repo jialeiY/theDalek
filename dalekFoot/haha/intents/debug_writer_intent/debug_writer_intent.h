5e248e64b2f972c58d4f7452adf3f90d21ab6e6a
HE Qichen <heqichen@gmail.com>
2024-08-20 00:01:58 +0800

    WIP: refactor debug data writer

--- dalekFoot/haha/intents/debug_writer_intent/debug_writer_intent.h
+++ dalekFoot/haha/intents/debug_writer_intent/debug_writer_intent.h
@@ -4,10 +4,57 @@
 
 #include <intents/intent_base.h>
 #include <mcap/writer.hpp>
+#include <string>
 
 
 namespace cooboc {
 namespace intent {
+
+
+class IMcapTopicConverter {
+  public:
+    IMcapTopicConverter(const std::string channelName) :
+        schema_ {nullptr},
+        channel_ {nullptr},
+        channelName_ {channelName} {};
+    IMcapTopicConverter(const IMcapTopicConverter &) = delete;
+    IMcapTopicConverter(IMcapTopicConverter &&)      = delete;
+    void operator=(const IMcapTopicConverter &)      = delete;
+    void operator=(IMcapTopicConverter &&)           = delete;
+    virtual ~IMcapTopicConverter() {
+        if (schema_ != nullptr) {
+            delete schema_;
+        }
+        if (channel_ != nullptr) {
+            delete channel_;
+        }
+    }
+    virtual void setup() = 0;
+
+  private:
+    mcap::Schema *schema_ {nullptr};
+    mcap::Channel *channel_ {nullptr};
+    const std::string channelName_;
+};
+
+template<typename T>
+class McapTopicCoonverter : public IMcapTopicConverter {
+  public:
+    McapTopicCoonverter(std::string channelName) : IMcapTopicConverter {channelName} {}
+    McapTopicCoonverter(const McapTopicCoonverter &) = delete;
+    McapTopicCoonverter(McapTopicCoonverter &&)      = delete;
+    void operator=(const McapTopicCoonverter &)      = delete;
+    void operator=(McapTopicCoonverter &&)           = delete;
+    virtual ~McapTopicCoonverter() {}
+    virtual void setup() {
+        schema_  = new mcap::Schema();
+        *schema_ = createSchema(T::descriptor());
+
+        channel_  = new mcap::Channel();
+        *channel_ = mcap::Channel()
+    }
+};
+
 class DebugWriterIntent : public IntentBase {
   public:
     DebugWriterIntent();
