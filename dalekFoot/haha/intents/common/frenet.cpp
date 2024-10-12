4ccac4c209297733f70cf950f99f3bb2e7a35c6d
HE Qichen <heqichen@gmail.com>
2024-10-12 08:51:43 +0800

    add comments

--- dalekFoot/haha/intents/common/frenet.cpp
+++ dalekFoot/haha/intents/common/frenet.cpp
@@ -8,7 +8,14 @@ namespace frenet {
 
 void locateSegmentInPolyline(const data::Position2D& poi,
                              const data::Position2D points[],
-                             const std::size_t pointNumber) {}
+                             const std::size_t pointNumber) {
+    // Find the segment..
+
+    // There are three possible that poi could located in
+    // 1. on the polyline
+    // 2. before the polyline
+    // 3. after the polyline
+}
 }    // namespace frenet
 }    // namespace intent
 }    // namespace cooboc
